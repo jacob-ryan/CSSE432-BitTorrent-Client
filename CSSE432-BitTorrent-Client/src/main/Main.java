5e0f077a7f02cf341f9359e34484d0cb4eb1abd5
Jacob Ryan <ryanjm@rose-hulman.edu>
2016-04-18 23:44:13 -0400

    Created basic Eclipse project for BitTorrent client

---
+++ CSSE432-BitTorrent-Client/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		new Main();
+	}
+	
+	public Main()
+	{
+		System.out.println("Does nothing...");
+	}
+}
---
+++ CSSE432-BitTorrent-Client/tests/main/MainTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class MainTest
+{
+	@Test
+	public void test()
+	{
+		fail("We have to write unit tests!");
+	}
+}
